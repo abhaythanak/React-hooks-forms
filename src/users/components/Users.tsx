c45df61d9e2885406ca8f3b02dec59471dff3917
abhaythanak <abhaythanak@gmail.com>
2024-08-31 23:13:29 +0530

    email validation using special character, schema implemented for structurize the input field values and there types

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,27 +1,23 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import {Stack, TextField} from '@mui/material';
+import { FormSchema, schema } from '../types/schema';
+import {zodResolver} from '@hookform/resolvers/zod'
+
 function Users() {
 
 const {register,
     formState:{errors,},
     handleSubmit,
-}= useForm<{email: string}>({mode:'all'});
+}= useForm<FormSchema>({mode:'all',resolver: zodResolver(schema)});
 
-function onFormSubmit(){
-    console.log("submmited")
-}
+
 
   return (
-    <form onSubmit={handleSubmit(onFormSubmit)}>
-      <input {...register('email', {
-        required : {value: true, message:"email is required"},
-        maxLength : {value: 20, message: "too many characters"},
-        minLength : {value:5, message: "minimum character required"}
-
-      })} placeholder='Enter email'/>
-
-      <p>{errors.email?.message}</p>
-    </form >
+    <Stack sx={{gap:2}}>
+    <TextField {...register('name')} label='Enter your name' error={!!errors.name} helperText={errors.name?.message}/>
+    <TextField {...register('email')} label='Enter your email' error={!!errors.email} helperText={errors.email?.message}/>
+    </Stack>
   )
 }
 
---
+++ src/users/types/schema.ts
@@ -0,0 +1,13 @@
+import {z} from 'zod'
+import { patterns } from '../../constants'
+
+export const schema = z.object({
+    name: z.string().min(3, {message: "Name is required"}),
+    email : z.string().min(1,"Email is required")
+    .refine((emailInput)=>{
+      return  patterns.email.test(emailInput)
+      
+    },{message: "Email is not valid"})
+})
+
+export type FormSchema = z.infer<typeof schema>
